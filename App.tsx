cb543c736e32349ad4969c1734a720c2732756e2
Yadu <cry13@Yadus-MacBook-Pro.local>
2021-03-26 16:48:44 +0530

    added redux persist

--- App.tsx
+++ App.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
 import store from "./src/redux/store";
 import RootNavigation from "./src/navigation/rootNavigation";
 
 export default function App() {
+  let persistor = persistStore(store);
+
   return (
     <Provider store={store}>
-      <SafeAreaView style={styles.container}>
-        <RootNavigation />
-      </SafeAreaView>
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeAreaView style={styles.container}>
+          <RootNavigation />
+        </SafeAreaView>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {
+  combineReducers,
+  configureStore,
+  getDefaultMiddleware,
+} from "@reduxjs/toolkit";
+import AsyncStorage from "@react-native-community/async-storage";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storiesSlicer from "./feature/storiesSlicer";
 
-export default configureStore({
-  reducer: {
-    stories: storiesSlicer,
-  },
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const reducers = combineReducers({
+  stories: storiesSlicer,
 });
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
+
+export default store;
